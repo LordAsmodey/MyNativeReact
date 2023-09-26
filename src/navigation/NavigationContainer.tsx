2298d13877c5b49908be99ced2e95f22aed7aa30
Oleg <oleg94038@gmail.com>
2023-09-26 13:25:00 +0200

    MNP-2 - Added navigation to app

---
+++ src/navigation/NavigationContainer.tsx
@@ -0,0 +1,9 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { RootStack } from './stacks/RootStack';
+export const Navigation = () => (
+  <NavigationContainer>
+    <RootStack />
+  </NavigationContainer>
+);
---
+++ src/screens/WelcomeScreen/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, VStack } from 'native-base';
+
+export const WelcomeScreen = () => {
+  return (
+    <VStack>
+      <Text>WelcomeScreen</Text>
+    </VStack>
+  );
+};
