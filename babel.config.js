9c179ac83815c61f844204e8c3fdbd3058bb08dc
Oleh Vietrov <104677837+LordAsmodey@users.noreply.github.com>
2023-12-11 17:33:25 +0200

    MNP-22: Added .env with types (#11)
    * Added .env with types

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,15 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        allowUndefined: true,
+      },
+    ],
+    [
       'module-resolver',
       {
         root: ['./src'],
---
+++ src/types/env.d.tsx
@@ -0,0 +1,7 @@
+// @ts-ignore
+// Added types for .env variables
+
+declare module '@env' {
+  export const API_URL: string;
+  export const API_URL_ANDROID: string;
+}
