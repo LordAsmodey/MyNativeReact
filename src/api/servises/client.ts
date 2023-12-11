111c7353d9ee73f9906b4cc09a1ca2e0bc1284f3
Oleg <oleg94038@gmail.com>
2023-12-11 16:28:04 +0100

    Added .env with types

--- src/api/servises/client.ts
+++ src/api/servises/client.ts
@@ -1,9 +1,10 @@
+import { API_URL, API_URL_ANDROID } from '@env';
 import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
 import { Platform } from 'react-native';
 
 import { tokenService } from './tokenService';
 
-const baseUrl = Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://10.0.2.2:3000';
+const baseUrl = Platform.OS === 'ios' ? API_URL : API_URL_ANDROID;
 
 // Implementation of a client service to add tokens to the request body and
 // intercept the server response if the user is not authorized. Will be used in the future.
